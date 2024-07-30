b05e17473e8157d7dbf4f927152e82559e147860
damarisfreg <fregosodmar24@gmail.com>
2024-07-29 23:59:10 -0700

    Working on tech balls and implementing more technology images

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -13,6 +13,18 @@ const Ball = (props) => {
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial 
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal 
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          flatShading
+          map={decal}
+        />
       </mesh>
       Ball
     </Float>
